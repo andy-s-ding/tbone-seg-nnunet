daba7445c24b3ad6a4c13ca49e2a24ae316321a8
Andy Ding <andy.s.ding@gmail.com>
2021-04-04 00:48:30 -0400

    excluded test set

--- tbone-segmentation/deformation_SSM/buildTransformSSM.m
+++ tbone-segmentation/deformation_SSM/buildTransformSSM.m
@@ -28,23 +28,32 @@ side = "RT";
 downsample_size = 60;
 template = 153;
 
+% Test split: ['RT_143', 'LT_148', 'LT_171', 'LT_147', 'LT_143', 'RT_147']
+
 side_files = dir(fullfile(transform_dir, sprintf('%s %d *-inverse-downsample%d.nii.gz', side, template, downsample_size)));
 other_side_files = dir(fullfile(transform_dir, sprintf('%s %d *-inverse-downsample%d-flipped.nii.gz', side, template, downsample_size)));
 
-num_transforms = length(side_files) + length(other_side_files);
+% Exclude any datasets in the test set
+exclude_side = ["143", "147"];
+exclude_other_side = ["148", "171", "147", "143"];
+side_names = string(vertcat(side_files(:).name));
+other_side_names = string(vertcat(other_side_files(:).name));
+side_names = exclude_indices(side_names, exclude_side);
+other_side_names = exclude_indices(other_side_names, exclude_side);
+
+num_transforms = length(side_names) + length(other_side_names);
 vectors = zeros(downsample_size^3*3,num_transforms);
 
-for i=1:length(side_files)
+for i=1:length(side_names)
     if i==1
-        name = fullfile(transform_dir, side_files(i).name);
-        deform_meta = load_nii(fullfile(transform_dir, side_files(i).name));
+        deform_meta = load_nii(char(fullfile(transform_dir, side_names(i))));
     end
-    curr_transform = niftiread(fullfile(transform_dir, side_files(i).name));
+    curr_transform = niftiread(fullfile(transform_dir, side_names(i)));
     vectors(:,i) = reshape(curr_transform, [numel(curr_transform), 1]);
 end
-for i=1:length(other_side_files)
-    curr_transform = niftiread(fullfile(transform_dir, other_side_files(i).name));
-    vectors(:,length(side_files)+i) = reshape(curr_transform, [numel(curr_transform), 1]);
+for i=1:length(other_side_names)
+    curr_transform = niftiread(fullfile(transform_dir, other_side_names(i)));
+    vectors(:,length(side_names)+i) = reshape(curr_transform, [numel(curr_transform), 1]);
 end
 
 [coeff,score,latent] = pca(vectors');
@@ -56,4 +65,21 @@ writeTransformH5(transform_H5_path, pcaMean, coeff, latent);
 transform_meta_path = fullfile(ssm_meta_dir, sprintf('%s %d inverse-downsample%d.mat', side, template, downsample_size));
 save(transform_meta_path, '-struct', 'deform_meta');
 
+end
+
+function names = exclude_indices(names, exclude_side)
+    excluded_indices = [];
+    for i=1:length(names(:,1))
+        curr_name_array = split(names(i,:));
+        curr_name = split(curr_name_array(3), '-');
+        curr_index = string(cell2mat(curr_name(1)));
+        excluded = any(exclude_side == curr_index);
+        if excluded
+            excluded_indices = [i, excluded_indices];
+        end
+    end
+    for i=1:length(excluded_indices)
+        index = excluded_indices(i);
+        names(index,:) = [];
+    end
 end
